b6754271b21732dbd20c405046602d66ef6a16e6
Curtis Clemens <cwclem@mit.edu>
2013-12-01 14:29:37 -0500

    Replace timestepper.

--- src/TimeStepper.cpp
+++ src/TimeStepper.cpp
@@ -1,30 +1,44 @@
 #include "TimeStepper.h"
 
+
+inline vector<Vector3f> mul(float c, vector<Vector3f> v)
+{
+	for(unsigned i=0; i<v.size(); i++) {
+		v[i] = c * v[i];
+	}
+	return v;
+}
+
+inline vector<Vector3f> add(vector<Vector3f> a, vector<Vector3f> b)
+{
+	for(unsigned i=0; i<a.size(); i++) {
+		a[i] = a[i] + b[i];
+	}
+	return a;
+}
+
 ///TODO: implement Explicit Euler time integrator here
 void ForwardEuler::takeStep(ParticleSystem* particleSystem, float stepSize)
 {
 	vector<Vector3f> state = particleSystem->getState();
-	vector<Vector3f> f = particleSystem->evalF(state);
-	vector<Vector3f> result;
-	for(int i = 0; i < f.size(); i++){
-		result.push_back(state[i] + stepSize * f[i]);
-	}
-	particleSystem->setState(result);
+	particleSystem->setState(add(state, mul(stepSize, particleSystem->evalF(state))));
 }
 
-///TODO: implement Trapezoidal rule here
-void Trapezoidal::takeStep(ParticleSystem* particleSystem, float stepSize)
+///TODO: implement Trapzoidal rule here
+void Trapzoidal::takeStep(ParticleSystem* particleSystem, float stepSize)
 {
 	vector<Vector3f> state = particleSystem->getState();
 	vector<Vector3f> f0 = particleSystem->evalF(state);
-	vector<Vector3f> temp;
-	for(int i = 0; i < f0.size(); i++){
-		temp.push_back(state[i] + stepSize * f0[i]);
-	}
-	vector<Vector3f> f1 = particleSystem->evalF(temp);
-	vector<Vector3f> result;
-	for(int i = 0; i < state.size(); i++){
-		result.push_back(state[i] + (stepSize/2) * (f0[i] + f1[i]));
-	}
-	particleSystem->setState(result);
+	vector<Vector3f> f1 = particleSystem->evalF(add(state, mul(stepSize, f0)));
+	particleSystem->setState(add(state, mul(0.5*stepSize, add(f0, f1))));
+}
+
+void RK4::takeStep(ParticleSystem* particleSystem, float stepSize)
+{
+	vector<Vector3f> state = particleSystem->getState();
+	vector<Vector3f> k1 = particleSystem->evalF(state);
+	vector<Vector3f> k2 = particleSystem->evalF(add(state, mul(stepSize/2.0f, k1)));
+	vector<Vector3f> k3 = particleSystem->evalF(add(state, mul(stepSize/2.0f, k2)));
+	vector<Vector3f> k4 = particleSystem->evalF(add(state, mul(stepSize, k3)));
+	particleSystem->setState(add(state, mul(stepSize/6.0f, add(k1, add(mul(2, k2), add(mul(2, k3), k4))))));
 }
