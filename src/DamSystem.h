d261331e6ec2ad61d86d6967fbd57ed87136e7a0
Curtis Clemens <cwclem@mit.edu>
2013-12-05 14:00:32 -0500

    Dam System fix.

--- src/DamSystem.h
+++ src/DamSystem.h
@@ -3,6 +3,7 @@
 
 #include "extra.h"
 #include <vector>
+#include <cmath>
 
 #include "FluidSystem.h"
 
@@ -29,7 +30,7 @@ public:
 
 		Vector3f diagonal = maxPoint - minPoint;
 		Vector3f setDiagonal = Vector3f(0.5*diagonal[0], 0.8*diagonal[1], diagonal[2]);
-		float r = (setDiagonal[0] + setDiagonal[1] + setDiagonal[2])/m_numParticles;		
+		float r = pow((setDiagonal[0] * setDiagonal[1] * setDiagonal[2])/m_numParticles, 1/3.0);		
 		int nx = setDiagonal[0]/r;
 		int ny = setDiagonal[1]/r;
 		int nz = m_numParticles/(nx*ny) + (m_numParticles%(nx*ny)==0? 0 : 1);
@@ -38,7 +39,7 @@ public:
 			for (int x=0; x<nx; x++) {
 				for (int z=0; z<nz; z++) {
 					if (nx*nz*y + nz*x + z+1 <= m_numParticles) {
-						Vector3f position = Vector3f(x*r, y*r, z*r);
+						Vector3f position = minPoint + Vector3f(x*r, y*r, z*r);
 						m_vVecState.push_back(position);
 						m_vVecState.push_back(Vector3f(0));
 						particles.push_back(FluidParticle(position, Vector3f()));
